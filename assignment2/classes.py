8009bca45264637c5cbe0207d2d6a0f6984af405
Gabi <gabriellafelgas@poli.ufrj.br>
2019-10-23 22:34:41 -0300

    Starting part 2

---
+++ assignment2/classes.py
@@ -0,0 +1,36 @@
+import cv2 
+import sys
+
+import numpy as np
+import matplotlib as plt
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
+class Image(object):
+
+    def __init__(self):
+        pass
+
+    def open(self):
+        pass
+
+    def save(self):
+        pass
+
+    def get_derivatives(self):
+        pass
+
+    def calc_covariance(self):
+        pass
+    
+    def get_corners(self):
+        pass
+
+    def coner_maxpooling(self):
+        pass
+
+    def corner_processing(self):
+        pass
+
+    
